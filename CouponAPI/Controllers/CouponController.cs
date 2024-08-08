747982b5456e67c7d8b4f833a0a020a7eee3524f
0xKolyanus <mukola.1zt.@gmail.com>
2024-08-08 14:33:31 +0300

    added coupon Controller

---
+++ CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,48 @@
+﻿using CouponAPI.Data;
+using CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u=>u.Id==id);
+                return obj;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
