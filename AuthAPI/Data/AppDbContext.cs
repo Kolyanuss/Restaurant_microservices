ad222755abc54aaab210dba16dffc400a68a8a46
0xKolyanus <mukola.1zt.@gmail.com>
2025-02-05 12:37:56 +0200

    Started AuthAPI project

---
+++ AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
