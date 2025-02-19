3bc9a0f96b5306705108cbf76f6f3bc0f6250adc
0xKolyanus <mukola.1zt.@gmail.com>
2025-02-19 10:33:01 +0200

    fix validation

--- ModelLibrary/Dto/LoginRequestDto.cs
+++ ModelLibrary/Dto/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace ModelLibrary.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLibrary.Dto
 {
     public class LoginRequestDto
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
--- ModelLibrary/Dto/RegistrationRequestDto.cs
+++ ModelLibrary/Dto/RegistrationRequestDto.cs
@@ -1,11 +1,18 @@
-﻿namespace ModelLibrary.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLibrary.Dto
 {
     public class RegistrationRequestDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string RoleName { get; set; }
     }
 }
