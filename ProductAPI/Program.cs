97eae1f62e1024da0d6813f4eb9802d017228dbc
0xKolyanus <mukola.1zt.@gmail.com>
2025-02-19 16:36:52 +0200

    setup db

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Services.ProductAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
+// controllers
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Auth
+// 
 
 var app = builder.Build();
 
@@ -20,4 +31,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+		if (_db.Database.GetPendingMigrations().Count() > 0)
+		{ _db.Database.Migrate(); }
+	}
+}
