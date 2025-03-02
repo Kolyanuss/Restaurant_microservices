f96deab375cfcbec38ed8e87f248fed3b0b1d69a
0xKolyanus <mukola.1zt.@gmail.com>
2025-03-02 20:19:58 +0200

    update ocelot.json config

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -3,18 +3,19 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddOcelot(builder.Configuration);
-
-builder.Services.AddControllers();
-
+    //.AddConsul();
 
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
